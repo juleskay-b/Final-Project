ff924d6798b89b1de8de01c2668ee8af8587b74b
juleskay-b <juliabush00@gmail.com>
2024-08-04 09:58:34 -0400

    Playability Update

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,6 @@
 #main function for the game
-import pygame, sys
+import sys
+import pygame
 from constants import *
 from sudoku_generator import *
 
@@ -124,25 +125,75 @@ def draw_numbers():
                 num9_rect = num9_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
                 screen.blit(num9_surf, num9_rect)
 
-def draw_game_over(screen):
+def select_box(col, row):
+    select_surface = pygame.Surface(size=(WIDTH//SIZE - 2, HEIGHT//SIZE - 2))
+    select_surface.fill(SELECT_COLOR)
+    screen.blit(select_surface, (col*BOX_SIZE + 3, row*BOX_SIZE + 3))
+    pygame.display.update()
+
+
+def fill_box(num, row, col):
+    number_font = pygame.font.Font(None, NUMBER_FONT)
+    add_num1_surf = number_font.render("1", 0, USER_NUMBER_COLOR)
+    add_num2_surf = number_font.render("2", 0, USER_NUMBER_COLOR)
+    add_num3_surf = number_font.render("3", 0, USER_NUMBER_COLOR)
+    add_num4_surf = number_font.render("4", 0, USER_NUMBER_COLOR)
+    add_num5_surf = number_font.render("5", 0, USER_NUMBER_COLOR)
+    add_num6_surf = number_font.render("6", 0, USER_NUMBER_COLOR)
+    add_num7_surf = number_font.render("7", 0, USER_NUMBER_COLOR)
+    add_num8_surf = number_font.render("8", 0, USER_NUMBER_COLOR)
+    add_num9_surf = number_font.render("9", 0, USER_NUMBER_COLOR)
+
+    if num == 1:
+        add_num1_rect = add_num1_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
+        screen.blit(add_num1_surf, add_num1_rect)
+    elif num == 2:
+        add_num2_rect = add_num2_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
+        screen.blit(add_num2_surf, add_num2_rect)
+    elif num == 3:
+        add_num3_rect = add_num3_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
+        screen.blit(add_num3_surf, add_num3_rect)
+    elif num == 4:
+        add_num4_rect = add_num4_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
+        screen.blit(add_num4_surf, add_num4_rect)
+    elif num == 5:
+        add_num5_rect = add_num5_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
+        screen.blit(add_num5_surf, add_num5_rect)
+    elif num == 6:
+        add_num6_rect = add_num6_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
+        screen.blit(add_num6_surf, add_num6_rect)
+    elif num == 7:
+        add_num7_rect = add_num7_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
+        screen.blit(add_num7_surf, add_num7_rect)
+    elif num == 8:
+        add_num8_rect = add_num8_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
+        screen.blit(add_num8_surf, add_num8_rect)
+    elif num == 9:
+        add_num9_rect = add_num9_surf.get_rect(center=(col * BOX_SIZE + BOX_SIZE / 2, row * BOX_SIZE + BOX_SIZE / 2))
+        screen.blit(add_num9_surf, add_num9_rect)
+
+    board[row][col] = num
+
+    pygame.display.update()
+
+
+def draw_game_over(screen, correct):
     game_over_font = pygame.font.Font(None, 40)
     screen.fill(BG_COLOR)
-    if completed:
+    if correct:
         text = "Level Completed"
     else:
         text = "Level Failed"
 
     game_over_surf = game_over_font.render(text, 0, LINE_COLOR)
-    game_over_rect = game_over.surf.get_rect(center=(WIDTH//2, HEIGHT//2 - 100))
+    game_over_rect = game_over_surf.get_rect(center=(WIDTH//2, HEIGHT//2 - 100))
     screen.blit(game_over_surf, game_over_rect)
 
-    restart_surf = game_over_font.render("Press R to restart the level...", 0, LINE_COLOR)
-    restart_rect = restart_surf.get_rect(center=(WIDTH//2, HEIGHT//2 + 100))
-    screen.blit(restart_surf, restart_rect)
-
-    menu_surf = game_over_font.render("Press M to return to the main menu...")
+    menu_surf = game_over_font.render("Press M to return to the main menu...", 0, LINE_COLOR)
     menu_rect = menu_surf.get_rect(center=(WIDTH//2, HEIGHT//2 + 150))
     screen.blit(menu_surf, menu_rect)
+
+    pygame.display.update()
 
 if __name__ == "__main__":
 
@@ -151,14 +202,15 @@ if __name__ == "__main__":
     pygame.display.set_caption("Sudoku")
     game_over = False
     completed = False
+    correct = True
 
     difficulty = draw_game_start(screen)
     if difficulty == "easy":
-        board = generate_sudoku(9, 30)
+        board = generate_sudoku(9, 2)
     elif difficulty == "medium":
-        board = generate_sudoku(9, 50)
+        board = generate_sudoku(9, 40)
     elif difficulty == "hard":
-        board = generate_sudoku(9, 70)
+        board = generate_sudoku(9, 50)
 
     screen.fill(BG_COLOR)
     draw_board()
@@ -170,8 +222,65 @@ if __name__ == "__main__":
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN and not completed:
                 x, y = event.pos
-                row = y // BOX_SIZE
-                col = x // BOX_SIZE
+                row = int(y // BOX_SIZE)
+                col = int(x // BOX_SIZE)
+                if board[row][col] == 0:
+                    select_box(col, row)
+            if event.type == pygame.KEYDOWN:
+                if pygame.key.get_pressed()[pygame.K_1]:
+                    fill_box(1, row, col)
+                elif pygame.key.get_pressed()[pygame.K_2]:
+                    fill_box(2, row, col)
+                elif pygame.key.get_pressed()[pygame.K_3]:
+                    fill_box(3, row, col)
+                elif pygame.key.get_pressed()[pygame.K_4]:
+                    fill_box(4, row, col)
+                elif pygame.key.get_pressed()[pygame.K_5]:
+                    fill_box(5, row, col)
+                elif pygame.key.get_pressed()[pygame.K_6]:
+                    fill_box(6, row, col)
+                elif pygame.key.get_pressed()[pygame.K_7]:
+                    fill_box(7, row, col)
+                elif pygame.key.get_pressed()[pygame.K_8]:
+                    fill_box(8, row, col)
+                elif pygame.key.get_pressed()[pygame.K_9]:
+                    fill_box(9, row, col)
+
+                if 0 not in board[0] and 0 not in board[1] and 0 not in board[2] and 0 not in board[3] and 0 not in board[4] and 0 not in board[5] and 0 not in board[6] and 0 not in board[7] and 0 not in board[8]:
+                    completed = True
+                    finished = SudokuGenerator(9, 0)
+                    finished.board = board
+                    for i in range(9):
+                        for j in range(9):
+                            if finished.is_valid(i, j, board[i][j]):
+                                continue
+                            else:
+                                correct = False
+                                break
+
+                    draw_game_over(screen, correct)
+                    pygame.display.update()
+
+                    if event.type == pygame.KEYDOWN:
+                        if pygame.key.get_pressed()[pygame.K_m]:
+                            correct = True
+                            completed = False
+
+                            draw_game_start(screen)
+                            pygame.display.update()
+
+
+
+
+
+
+
+                for i in board:
+                    for j in i:
+                        print(j, end=" ")
+                    print("")
+                print("")
+
 
 
         pygame.display.update()
